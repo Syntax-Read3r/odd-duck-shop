22bce6e86cc66d1f66bb1698b0894e919be2a04e
Munyaradzi Chikara <SyntaxRead3r@gmail.com>
2022-06-19 01:29:08 +0100

    cart draft one

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,7 @@
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 const table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
-let cart;
+renderCart();
 
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
@@ -13,33 +13,61 @@ function loadCart() {
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
 function renderCart() {
+  
   loadCart();
   clearCart();
   showCart();
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+[x]
+function clearCart() {
+  const cartRows = document.getElementById('cart-rows');
+  cartRows.innerHTML = '';
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
-  // TODO: Find the table body
-
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
-
+  const cartRows = document.getElementById('cart-rows');
+  cart.items.forEach(item => {
+    const row = document.createElement('tr');
+    row.innerHTML = ` <td>${item.name}</td> <td>${item.price}</td> <td>${item.quantity}</td> <td>${item.price * item.quantity}</td> <td><a href="#" class="delete">X</a></td>`;
+    cartRows.appendChild(row);
+  }
+  );
 }
 
+// TODO: Find the table body
+[x]
+
+// TODO: Iterate over the items in the cart
+[x]
+// TODO: Create a TR
+[x]
+// TODO: Create a TD for the delete link, quantity,  and the item
+[x]
+// TODO: Add the TR to the TBODY and each of the TD's to the TR
+[x]
+
+
+// TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
 function removeItemFromCart(event) {
-
-  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
-  // TODO: Save the cart back to local storage
-  // TODO: Re-draw the cart table
-
+  if (event.target.className === 'delete') {
+    const name = event.target.parentElement.parentElement.firstElementChild.textContent;
+    cart.removeItem(name);
+    cart.saveToLocalStorage();
+    renderCart();
+  }
 }
+
+
+
+// TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+[x]
+// TODO: Save the cart back to local storage
+[x]
+// TODO: Re-draw the cart table
+[x]
 
 // This will initialize the page and draw the cart on screen
 renderCart();
